256d8e12e6baed411522f9fdc02088c59f86d65c
Unknown <angelovandenbroek@hotmail.com>
2019-09-30 11:31:14 +0200

    bullet counter added (no function to target hit)

---
+++ Duckhunt-v1.0.0/Assets/Scripts/BulletCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletCounter : MonoBehaviour
+
+   
+{
+    public Text bulletText;
+    public int bullets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullets = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletText.text = "Bullets:" + bullets;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!(bullets <= 0))
+            {
+                bullets--;
+            }
+        }
+        
+    }
+}
