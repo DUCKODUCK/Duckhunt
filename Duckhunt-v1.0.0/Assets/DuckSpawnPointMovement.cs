5097953425f5ca6635491ed6775f80d0218aa9db
Glenn Meering <glenn.dev@hotmail.com>
2019-09-30 09:12:48 +0200

    testing on duckspawning

---
+++ Duckhunt-v1.0.0/Assets/DuckSpawnPointMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpawnPointMovement : MonoBehaviour
+{
+    int screenWitdh;
+    float movementSpeed;
+    float duckPositionX;
+    string direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWitdh = Screen.width;
+        movementSpeed = 1f;
+        direction = "right";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        duckPositionX = DuckSpawnPoint.position.X;
+
+        if (duckPositionX == screenWitdh)
+        {
+            direction = "left";
+        }
+        else if (duckPositionX == 0f)
+        {
+            direction = "right";
+        }
+
+        if (direction == "right")
+        {
+            DuckSpawnPoint.position.X = 0;
+            DuckSpawnPoint.position.X = DuckSpawnPoint.position.X + movementSpeed;
+        }
+        else if (direction == "left")
+        {
+            DuckSpawnPoint.position.X = 0;
+            DuckSpawnPoint.position.X = DuckSpawnPoint.position.X - movementSpeed;
+        }
+    }
+}
