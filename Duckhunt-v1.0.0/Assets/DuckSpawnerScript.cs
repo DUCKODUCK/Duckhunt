7f66c62ea7290bf7d77119ae232d950dfd3aa968
Unknown <angelovandenbroek@hotmail.com>
2019-10-02 16:53:32 +0200

    Added random spawning

---
+++ Duckhunt-v1.0.0/Assets/DuckSpawnerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpawnerScript : MonoBehaviour
+{
+    public GameObject Duck;
+    float randX;
+    Vector3 whereToSpawn;
+    public float spawnrate = 2.5f;
+    float nextSpawn = 2f;
+    public float currentTime;
+    bool spawnBird;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime < nextSpawn)
+        {
+            currentTime += 1.0f * Time.deltaTime;
+            spawnBird = false;
+        }
+        else
+        {
+            currentTime = 0f;
+            spawnBird = true;
+        }
+        Debug.Log(Time.time + " " + nextSpawn + " - " + spawnrate);
+        if (spawnBird)
+        {
+
+
+            randX = Random.Range(-8.4f, 8.4f);
+            whereToSpawn = new Vector3(randX, transform.position.y);
+            Instantiate(Duck, whereToSpawn, Quaternion.identity);
+            spawnBird = false;
+
+
+        }
+    }
+}
--- Duckhunt-v1.0.0/Assets/Scripts/PaternBirds.cs
+++ Duckhunt-v1.0.0/Assets/Scripts/PaternBirds.cs
@@ -8,12 +8,6 @@ public class PaternBirds : MonoBehaviour
 
     public float speed = .05f;
     public bool direction = true;
-    public GameObject Duck;
-    float randX;
-    Vector3 whereToSpawn;
-    public float spawnrate = 2f;
-    float nextSpawn = 0.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +15,9 @@ public class PaternBirds : MonoBehaviour
     }
 
 
-    [Header("X Spawn Range")]
-    public float xMin;
-    public float xMax;
-
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextSpawn)
-        {
-            nextSpawn = Time.time + spawnrate;
-            randX = Random.Range(-8.4f, 8.4f);
-            whereToSpawn = new Vector3(randX, transform.position.y);
-            Instantiate(Duck, whereToSpawn, Quaternion.identity);
-        }
         if (direction)
         {
             transform.position += new Vector3(speed, speed);
@@ -57,4 +40,5 @@ public class PaternBirds : MonoBehaviour
     {
         direction = (Random.value < .5);
     }
+
 }
