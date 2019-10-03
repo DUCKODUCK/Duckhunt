1adfc00c76d794956def60f4a79f995b56e018b9
Glenn Meering <glenn.dev@hotmail.com>
2019-10-03 08:38:08 +0200

    validation on products

---
+++ Duckhunt-v1.0.0/Assets/Scripts/KillEnemyOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemyOnClick : MonoBehaviour
+{
+
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Ray ray = cam.ScreenPointToRay(new Vector2(200, 200));
+            //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+            //Debug.Log(ray);
+        }
+    }
+}
