156d58eb1eda6560522f6eedc3e321a3a0f11383
Unknown <angelovandenbroek@hotmail.com>
2019-10-10 12:19:46 +0200

    did so much

---
+++ Duckhunt-v1.0.0/Assets/Scripts/poinitScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class poinitScript : MonoBehaviour
+{
+
+    public Text pointText;
+   // public GameObject duck;
+    public int points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+        pointText.text = "Points:";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "Points:" + points.ToString();
+    }
+}
